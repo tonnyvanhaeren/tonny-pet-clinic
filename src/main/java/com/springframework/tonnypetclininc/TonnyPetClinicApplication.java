4c6e27a773471414a092d96bea17ccd24c648d01
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-01-10 16:16:58 +0100

    Initial Project Add

---
+++ src/main/java/com/springframework/tonnypetclininc/TonnyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.tonnypetclininc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonnyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonnyPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/tonnypetclininc/TonnyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.tonnypetclininc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonnyPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
