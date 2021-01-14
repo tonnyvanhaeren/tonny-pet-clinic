65f183ebafc22f719593788afb7873445fdc8c87
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-01-14 10:40:40 +0100

    Refactoring to use common crud interface. Closes #11

--- pet-clinic-data/src/main/java/tonnypetclininc/services/OwnerService.java
+++ pet-clinic-data/src/main/java/tonnypetclininc/services/OwnerService.java
@@ -2,13 +2,6 @@ package tonnypetclininc.services;
 
 import tonnypetclininc.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastname);
-    Owner findById(long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
-
 }
--- pet-clinic-data/src/main/java/tonnypetclininc/services/VetService.java
+++ pet-clinic-data/src/main/java/tonnypetclininc/services/VetService.java
@@ -1,10 +1,6 @@
 package tonnypetclininc.services;
 
 import tonnypetclininc.model.Vet;
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
