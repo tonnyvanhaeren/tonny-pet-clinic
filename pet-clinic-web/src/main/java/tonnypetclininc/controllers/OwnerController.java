0aa0e8b8d806dc5b1600e7a64dccd98e6884ac4d
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-01-28 08:53:07 +0100

    Fix display error on Owner. Closes #61

--- pet-clinic-web/src/main/java/tonnypetclininc/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tonnypetclininc/controllers/OwnerController.java
@@ -2,7 +2,12 @@ package tonnypetclininc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import tonnypetclininc.model.Owner;
+import tonnypetclininc.model.Pet;
 import tonnypetclininc.services.OwnerService;
 
 @Controller
@@ -24,6 +29,19 @@ public class OwnerController {
     @RequestMapping({"/find"})
     public String FindOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+
+//        for (Pet pet : owner.getPets()) {
+//            pet.setVisitsInternal(visits.findByPetId(pet.getId()));
+//        }
+//
+//        mav.addObject(owner);
+        return mav;
     }
 
 }
